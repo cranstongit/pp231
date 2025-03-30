c2981f52318e61771ad4454c372c8fb1d901825d
cranstongit <alexbludin@gmail.com>
2025-03-31 00:12:47 +0300

    Innitial commit for the task 231. The 221 task was taken as a base for the CRUD app.

---
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,34 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImp implements UserService {
+
+   @Autowired
+   private UserDao userDaoUser;
+
+   @Transactional
+   @Override
+   public void deleteUserTable() {
+      userDaoUser.deleteUserTable();
+   }
+
+   @Transactional
+   @Override
+   public void add(User user) {
+      userDaoUser.add(user);
+   }
+
+   @Override
+   public List<User> listUsers() {
+      return userDaoUser.listUsers();
+   }
+}
