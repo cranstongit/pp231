2dc0dfe7fc4dd60eba349e3d271f2968f9929cec
Alex Bludin <alexbludin@gmail.com>
2025-04-07 17:53:46 +0300

    Add new user controller methods and pages created.

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -3,8 +3,10 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import web.model.User;
 import web.service.UserService;
 
 @Controller
@@ -18,8 +20,59 @@ public class UsersController {
     }
 
     @GetMapping //browser address
-    public String printUsers(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
-            model.addAttribute("getUsers", userService.findAll()); //attribute in the users.html
-        return "/users"; //spring will be looking for a users.html file
+    public String printUsers(ModelMap model) {
+            model.addAttribute("getUsers", userService.findAll()); //attribute in the index.html
+        return "/index"; //spring will be looking for an index.html file
     }
+
+    @GetMapping("/newuser")
+    public String newUser(ModelMap model) {
+        model.addAttribute("newUser", new User());
+        return "/new";
+    }
+
+    @PostMapping
+    public String newUser(@ModelAttribute("newUser") User user) {
+        userService.save(user);
+        return "redirect:/";
+    }
+
+//    @PostMapping()
+//    public String create(@ModelAttribute("person") @Valid Person person,
+//                         BindingResult bindingResult) {
+//        if (bindingResult.hasErrors())
+//            return "people/new";
+//
+//        peopleService.save(person);
+//        return "redirect:/people";
+//    }
+//
+//    @GetMapping
+//    public String edit(Model model, @PathVariable("id") int id) {
+//        model.addAttribute("editUser", userService.findOne(id));
+//        return "/users";
+//    }
+//
+//    @PatchMapping("/{id}")
+//    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+//                         @PathVariable("id") int id) {
+//        if (bindingResult.hasErrors())
+//            return "people/edit";
+//
+//        peopleService.update(id, person);
+//        return "redirect:/people";
+//    }
+//
+//    @DeleteMapping
+//    public String delete(@PathVariable("id") int id) {
+//        peopleService.delete(id);
+//        return "/users";
+//    }
+
+//    @GetMapping
+//    public String method(@RequestParam(value = "id", required = false) Long id, @RequestParam(value = "name", required = false, Model model) String name) {
+//        model.addAtribute("") //кладем пару ключ значение в атрибут
+//        return "/uses";
+//    }
+
 }
